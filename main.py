fc9383e7d77f2afb6f4572e5a0912b7c70f356a2
DBojneagu <93039914+DBojneagu@users.noreply.github.com>
2022-02-23 10:35:25 +0200

    update

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import numpy as np
 import sys
 
 #singura sansa la python sort e sa dai nr de numere minim 1000
-#noteaza faptul ca quicksort la 1000 nu merge pe 1000 de numere, dar la recursionlimit 1500 merge, iar pe 100 de numere merge oricum, dupa nu mai merge quicksort.
-
+#noteaza faptul ca quicksort la 1000 nu merge pe 1000 de numere,
+# dar la recursionlimit 1500 merge, iar pe 100 de numere merge oricum, dupa nu mai merge quicksort.
+#Daca dureaza prea mult, facem error handling si afisam
+# spre exemplu ca dureaza prea mult, iar la quicksort incercam sa l facem iterativ
 
 def partition(array, low, high):
 
@@ -33,50 +35,61 @@ def partition(array, low, high):
   return i + 1
 
 
-def quickSort(array, low, high):
-  if low < high:
 
-    # find pivot element such that
-    # element smaller than pivot are on the left
-    # element greater than pivot are on the right
-    pi = partition(array, low, high)
+def partition(array, low, high):
 
-    # recursive call on the left of pivot
-    quickSort(array, low, pi - 1)
 
-    # recursive call on the right of pivot
-    quickSort(array, pi + 1, high)
+  pivot = array[high]
+
+
+  i = low - 1
+
+
+  for j in range(low, high):
+    if array[j] <= pivot:
+
+      i = i + 1
+
+
+      (array[i], array[j]) = (array[j], array[i])
+
+
+  (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+
+  return i + 1
+
+
+
 
 def heapify(arr, n, i):
-  largest = i  # Initialize largest as root
-  l = 2 * i + 1  # left = 2*i + 1
-  r = 2 * i + 2  # right = 2*i + 2
+  largest = i
+  l = 2 * i + 1
+  r = 2 * i + 2  #
 
-  # See if left child of root exists and is
-  # greater than root
+
   if l < n and arr[largest] < arr[l]:
     largest = l
 
-  # See if right child of root exists and is
-  # greater than root
+
   if r < n and arr[largest] < arr[r]:
     largest = r
 
-  # Change root, if needed
-  if largest != i:
-    arr[i], arr[largest] = arr[largest], arr[i]  # swap
 
-    # Heapify the root.
+  if largest != i:
+    arr[i], arr[largest] = arr[largest], arr[i]
+
+
     heapify(arr, n, largest)
 
 def heapSort(arr):
   n = len(arr)
 
-  # Build a maxheap.
+
   for i in range(n // 2 - 1, -1, -1):
     heapify(arr, n, i)
 
-  # One by one extract elements
+
   for i in range(n - 1, 0, -1):
     arr[i], arr[0] = arr[0], arr[i]  # swap
     heapify(arr, i, 0)
@@ -116,14 +129,13 @@ def mergeSort(arr):
             k += 1
 
 def shellSort(arr):
-    gap = len(arr) // 2  # initialize the gap
+    gap = len(arr) // 2
 
     while gap > 0:
         i = 0
         j = gap
 
-        # check the array in from left to right
-        # till the last possible index of j
+
         while j < len(arr):
 
             if arr[i] > arr[j]:
@@ -132,8 +144,7 @@ def shellSort(arr):
             i += 1
             j += 1
 
-            # now, we look back from ith index to the left
-            # we swap the values which are not in the right order.
+
             k = i
             while k - gap > -1:
 
@@ -146,23 +157,22 @@ def shellSort(arr):
 def countingSort(arr, exp1):
     n = len(arr)
 
-    # The output array elements that will have sorted arr
+
     output = [0] * (n)
 
-    # initialize count array as 0
+
     count = [0] * (10)
 
-    # Store count of occurrences in count[]
+
     for i in range(0, n):
         index = arr[i] // exp1
         count[index % 10] += 1
 
-    # Change count[i] so that count[i] now contains actual
-    # position of this digit in output array
+
     for i in range(1, 10):
         count[i] += count[i - 1]
 
-    # Build the output array
+
     i = n - 1
     while i >= 0:
         index = arr[i] // exp1
@@ -170,22 +180,19 @@ def countingSort(arr, exp1):
         count[index % 10] -= 1
         i -= 1
 
-    # Copying the output array to arr[],
-    # so that arr now contains sorted numbers
+
     i = 0
     for i in range(0, len(arr)):
         arr[i] = output[i]
 
 
 def insertionSort(arr):
-  # Traverse through 1 to len(arr)
+
   for i in range(1, len(arr)):
 
     key = arr[i]
 
-    # Move elements of arr[0..i-1], that are
-    # greater than key, to one position ahead
-    # of their current position
+
     j = i - 1
     while j >= 0 and key < arr[j]:
       arr[j + 1] = arr[j]
@@ -194,30 +201,64 @@ def insertionSort(arr):
 
 
 def radixSort(arr):
-    # Find the maximum number to know number of digits
+
     max1 = max(arr)
 
-    # Do counting sort for every digit. Note that instead
-    # of passing digit number, exp is passed. exp is 10^i
-    # where i is current digit number
+
     exp = 1
     while max1 / exp > 1:
         countingSort(arr, exp)
         exp *= 10
 
 
+def partition(start, end, array):
+
+    pivot_index = start
+    pivot = array[pivot_index]
+
+
+    while start < end:
+
+
+        while start < len(array) and array[start] <= pivot:
+            start += 1
+
+
+        while array[end] > pivot:
+            end -= 1
+
+
+        if (start < end):
+            array[start], array[end] = array[end], array[start]
+
+
+    array[end], array[pivot_index] = array[pivot_index], array[end]
+
+
+    return end
+
+
+
+def quick_sort(start, end, array):
+    if (start < end):
+
+        p = partition(start, end, array)
+
+
+        quick_sort(start, p - 1, array)
+        quick_sort(p + 1, end, array)
+
+
 def bubbleSort(arr):
   n = len(arr)
 
-  # Traverse through all array elements
+
   for i in range(n):
 
-    # Last i elements are already in place
+
     for j in range(0, n - i - 1):
 
-      # traverse the array from 0 to n-i-1
-      # Swap if the element found is greater
-      # than the next element
+
       if arr[j] > arr[j + 1]:
         arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
@@ -272,16 +313,7 @@ for teste in f:
     arr = aux1
     print("Timp Radix Sort: ", stop-start , "secunde", end="\n\n")
 
-    # QuickSort
-    #if int(aux[0]) < 1000:
-     #  start = time.time()
-      # quickSort(arr, 0, len(arr) - 1)
-       #stop = time.time()
-      # arr = aux1
-       #print("Timp Quick Sort: ", stop - start, "secunde", end="\n\n")
-   # else:
-    #  print("Quicksort nu poate")
-    #  print()
+
 
     # HeapSort
     start = time.time()
@@ -304,8 +336,16 @@ for teste in f:
     arr = aux1
     print("Timp Bubble Sort:", stop - start, "secunde", end="\n\n")
 
+    # Quick Sort
+    if int(aux[0]) < 10000:
+         start = time.time()
+         quick_sort(0, len(arr) - 1, arr)
+         stop = time.time()
+         arr = aux1
+         print("Timp Quick Sort:", stop - start, "secunde", end="\n\n")
+
     # ShellSort
-    if int(aux[0]) < 10001:
+    if int(aux[0]) < 10000:
       start = time.time()
       shellSort(arr)
       stop = time.time()
