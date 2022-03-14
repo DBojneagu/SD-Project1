5772217b3283aabb076fce701bb0d64029267c98
DBojneagu <93039914+DBojneagu@users.noreply.github.com>
2022-03-14 13:24:04 +0200

    update

--- main.py
+++ main.py
@@ -2,37 +2,24 @@ import random
 import time
 import numpy as np
 import sys
-
-# singura sansa la python sort e sa dai nr de numere minim 1000
-# noteaza faptul ca quicksort la 1000 nu merge pe 1000 de numere,
-# dar la recursionlimit 1500 merge, iar pe 100 de numere merge oricum, dupa nu mai merge quicksort.
-# Daca dureaza prea mult, facem error handling si afisam
-# spre exemplu ca dureaza prea mult, iar la quicksort incercam sa l facem iterativ
+import copy
 
 
 def partition(array, low, high):
 
-    # choose the rightmost element as pivot
     pivot = array[high]
 
-    # pointer for greater element
     i = low - 1
 
-    # traverse through all elements
-    # compare each element with pivot
     for j in range(low, high):
         if array[j] <= pivot:
-            # if element smaller than pivot is found
-            # swap it with the greater element pointed by i
+
             i = i + 1
 
-            # swapping element at i with element at j
             (array[i], array[j]) = (array[j], array[i])
 
-    # swap the pivot element with the greater element specified by i
     (array[i + 1], array[high]) = (array[high], array[i + 1])
 
-    # return the position from where partition is done
     return i + 1
 
 
@@ -57,9 +44,9 @@ def partition(array, low, high):
 def heapify(arr, n, i):
     largest = i
     l = 2 * i + 1
-    r = 2 * i + 2  #
+    r = 2 * i + 2
 
-    if l < n and arr[largest] < arr[l]:
+    if l < n and arr[i] < arr[l]:
         largest = l
 
     if r < n and arr[largest] < arr[r]:
@@ -67,18 +54,18 @@ def heapify(arr, n, i):
 
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
-
         heapify(arr, n, largest)
 
 
 def heapSort(arr):
     n = len(arr)
 
-    for i in range(n // 2 - 1, -1, -1):
+    for i in range(n // 2, -1, -1):
         heapify(arr, n, i)
 
     for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # swap
+        arr[i], arr[0] = arr[0], arr[i]
+
         heapify(arr, i, 0)
 
 
@@ -117,96 +104,68 @@ def mergeSort(arr):
             k += 1
 
 
-def shellSort(arr):
-    gap = len(arr) // 2
+def shellSort(array, n):
 
-    while gap > 0:
-        i = 0
-        j = gap
+    # Rearrange elements at each n/2, n/4, n/8, ... intervals
+    interval = n // 2
+    while interval > 0:
+        for i in range(interval, n):
+            temp = array[i]
+            j = i
+            while j >= interval and array[j - interval] > temp:
+                array[j] = array[j - interval]
+                j -= interval
 
-        while j < len(arr):
-
-            if arr[i] > arr[j]:
-                arr[i], arr[j] = arr[j], arr[i]
-
-            i += 1
-            j += 1
-
-            k = i
-            while k - gap > -1:
-
-                if arr[k - gap] > arr[k]:
-                    arr[k - gap], arr[k] = arr[k], arr[k - gap]
-                k -= 1
-
-        gap //= 2
+            array[j] = temp
+        interval //= 2
 
 
-def insertionSort(arr):
-
-    for i in range(1, len(arr)):
-
-        key = arr[i]
-
-        j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
+def insertionSort(array):
+    for step in range(1, len(array)):
+        key = array[step]
+        j = step - 1
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j = j - 1
+        array[j + 1] = key
 
 
-def countingSortForRadix(inputArray, placeValue):
-    # We can assume that the number of digits used to represent
-    # all numbers on the placeValue position is not grater than 10
-    countArray = [0] * 10
-    inputSize = len(inputArray)
+def countingSort(array, place):
+    size = len(array)
+    output = [0] * size
+    count = [0] * 10
 
-    # placeElement is the value of the current place value
-    # of the current element, e.g. if the current element is
-    # 123, and the place value is 10, the placeElement is
-    # equal to 2
-    for i in range(inputSize):
-        placeElement = (inputArray[i] // placeValue) % 10
-        countArray[placeElement] += 1
+    # Calculate count of elements
+    for i in range(0, size):
+        index = array[i] // place
+        count[index % 10] += 1
 
+    # Calculate cumulative count
     for i in range(1, 10):
-        countArray[i] += countArray[i - 1]
+        count[i] += count[i - 1]
 
-    # Reconstructing the output array
-    outputArray = [0] * inputSize
-    i = inputSize - 1
+    # Place the elements in sorted order
+    i = size - 1
     while i >= 0:
-        currentEl = inputArray[i]
-        placeElement = (inputArray[i] // placeValue) % 10
-        countArray[placeElement] -= 1
-        newPosition = countArray[placeElement]
-        outputArray[newPosition] = currentEl
+        index = array[i] // place
+        output[count[index % 10] - 1] = array[i]
+        count[index % 10] -= 1
         i -= 1
 
-    return outputArray
+    for i in range(0, size):
+        array[i] = output[i]
 
 
-def radixSort(inputArray):
-    # Step 1 -> Find the maximum element in the input array
-    maxEl = max(inputArray)
+# Main function to implement radix sort
+def radixSort(array):
+    # Get maximum element
+    max_element = max(array)
 
-    # Step 2 -> Find the number of digits in the `max` element
-    D = 1
-    while maxEl > 0:
-        maxEl /= 10
-        D += 1
-
-    # Step 3 -> Initialize the place value to the least significant place
-    placeVal = 1
-
-    # Step 4
-    outputArray = inputArray
-    while D > 0:
-        outputArray = countingSortForRadix(outputArray, placeVal)
-        placeVal *= 10
-        D -= 1
-
-    return outputArray
+    # Apply counting sort to sort elements based on place value.
+    place = 1
+    while max_element // place > 0:
+        countingSort(array, place)
+        place *= 10
 
 
 def partition(start, end, array):
@@ -239,15 +198,15 @@ def quick_sort(start, end, array):
         quick_sort(p + 1, end, array)
 
 
-def bubbleSort(arr):
-    n = len(arr)
+def bubbleSort(array):
+    for i in range(len(array)):
 
-    for i in range(n):
+        for j in range(0, len(array) - i - 1):
 
-        for j in range(0, n - i - 1):
-
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
 
 
 c = 0
@@ -259,87 +218,83 @@ for teste in f:
 
     aux = teste.split()
 
-    arr = [random.randrange(1, int(aux[1])) for i in range(int(aux[0]))]
-    aux1 = arr
-    # arr = list(np.random.randint(low = 0,high=int(aux[1]),size=int(aux[0])))
+    prearr = [random.randrange(1, int(aux[1])) for i in range(int(aux[0]))]
+    arr = prearr
+    auxMerge = copy.deepcopy(prearr)
+    auxRadix = copy.deepcopy(prearr)
+    auxHeap = copy.deepcopy(prearr)
+    auxShell = copy.deepcopy(prearr)
+    auxBubble = copy.deepcopy(prearr)
+    auxInsertion = copy.deepcopy(prearr)
 
     c += 1
-    print(f"Sir {c} \n N={int(aux[0])} Max = {int(aux[1])} \n ")
-    print("Sir initial ", end="\n")
-    # printList(arr)
 
-    # Built-in Sorting Algorithm
-    start = time.time()
-    arr3 = sorted(arr)
-    stop = time.time()
-    arr = aux1
-    print("Timp Merge sort: ", stop - start, "secunde", end="\n\n")
+    print(f"Sir {c} \n N={int(aux[0])} Max = {int(aux[1])} \n ")
 
     # MergeSort
+    arr = auxMerge
     start = time.time()
     mergeSort(arr)
     stop = time.time()
-    if arr == arr3:
-        print(f"Correct:{arr==arr3}")
-        print()
+    if arr == sorted(arr):
+        print(f"Correct = True")
+    else:
+        print(f"Correct = False")
     print("Timp Merge sort: ", stop - start, "secunde", end="\n\n")
-    arr = aux1
 
+    # Radix Sort
+    arr = auxRadix
     start = time.time()
     radixSort(arr)
     stop = time.time()
-    if arr == arr3:
-        print(f"Correct:{arr==arr3}")
-        print()
+    if arr == sorted(arr):
+        print(f"Correct = True")
+    else:
+        print(arr)
+        print(f"Correct = False")
     print("Timp Radix sort: ", stop - start, "secunde", end="\n\n")
-    arr = aux1
 
+    # Heap Sort
+    arr = auxHeap
     start = time.time()
     heapSort(arr)
     stop = time.time()
-    if arr == arr3:
-        print(f"Correct:{arr==arr3}")
-        print()
+    if arr == sorted(arr):
+        print(f"Correct = True")
+    else:
+        print(f"Correct = False")
     print("Timp Heap sort: ", stop - start, "secunde", end="\n\n")
-    arr = aux1
 
+    # Insertion Sort
+    arr = auxInsertion
     start = time.time()
     insertionSort(arr)
     stop = time.time()
-
-    if arr == arr3:
-        print(f"Correct:{arr==arr3}")
-        print()
+    if arr == sorted(arr):
+        print(f"Correct = True")
+    else:
+        print(f"Correct = False")
     print("Timp Insertion sort: ", stop - start, "secunde", end="\n\n")
-    arr = aux1
 
+    # Bubble Sort
+    arr = auxBubble
     start = time.time()
     bubbleSort(arr)
-
     stop = time.time()
-    if arr == arr3:
-        print(f"Correct:{arr==arr3}")
-        print()
-    print("Timp Bubble sort: ", stop - start, "secunde", end="\n\n")
-    arr = aux1
+    if arr == sorted(arr):
+        print(f"Correct = True")
+    else:
+        print(f"Correct = False")
 
-    if int(aux[0]) < 10000:
-        start = time.time()
-        quick_sort(0, len(arr) - 1, arr)
-        stop = time.time()
-        print("Timp Quick Sort:", stop - start, "secunde", end="\n\n")
-    else:
-        print("Quicksort da Recursion Error")
-        print()
+    print("Timp Bubble sort: ", stop - start, "secunde", end="\n\n")
+
     # ShellSort
-    if int(aux[0]) < 10000:
-        start = time.time()
-        shellSort(arr)
-        stop = time.time()
-        if arr == arr3:
-            print(f"Correct:{arr==arr3}")
-            print()
-        print("Timp Shell sort: ", stop - start, "secunde", end="\n\n")
-        arr = aux1
+    arr = auxShell
+    start = time.time()
+    shellSort(arr, len(arr))
+    stop = time.time()
+    if arr == sorted(arr):
+        print(f"Correct = True")
     else:
-        print("Dureaza prea mult Shell-Sort")
+        print(f"Correct = False")
+    print("Timp Shell sort: ", stop - start, "secunde", end="\n\n")
